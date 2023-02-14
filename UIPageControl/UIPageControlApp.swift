46bc26accb149254868f41c2fa77296705d3d0d7
Ikmal Azman <opikmalazman@gmail.com>
2023-02-14 13:50:38 +0000

    Initial Commit

---
+++ UIPageControl/UIPageControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPageControlApp.swift
+//  UIPageControl
+//
+//  Created by Ikmal Azman on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct UIPageControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
