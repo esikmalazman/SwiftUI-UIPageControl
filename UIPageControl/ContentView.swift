da2a832a96e2185ce639808abdb765ce623d100a
Ikmal Azman <opikmalazman@gmail.com>
2023-02-20 18:03:38 +0000

    implement uipagecontrol wrapper along with simple image carousel & button

--- UIPageControl/ContentView.swift
+++ UIPageControl/ContentView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentPageIndex : Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                if currentPageIndex == 0 {
+                    Image("image1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 300)
+                    
+                    
+                } else if currentPageIndex == 1 {
+                    Image("image2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 300)
+                    
+                    
+                } else {
+                    Image("image3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 300)
+                }
+                
+            }
+            
+            PageControl(currentPageIndex: currentPageIndex, numberOfPages: 3)
+            
+            HStack {
+                Button {
+                    if currentPageIndex != 0 {
+                        currentPageIndex = currentPageIndex - 1
+                    }
+                } label: {
+                    Text("Previous")
+                    
+                }
+                .buttonStyle(.borderless)
+                .padding()
+                
+                Button {
+                    if currentPageIndex < 3 {
+                        currentPageIndex = currentPageIndex + 1
+                    }
+                } label: {
+                    Text("Next")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+            }
+            
         }
+        .animation(.spring())
         .padding()
     }
 }
